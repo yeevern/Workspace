f5586c5176ee244b25b080b9b4308858c0f9f414
yeevern <yeevernchang@gmail.com>
2023-11-08 14:04:57 +0800

    add asgn1 of js

--- JS/image-gallery/main.js
+++ JS/image-gallery/main.js
@@ -5,14 +5,39 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const arr = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altArr = ['Closeup of a human eye', 
+                'pic2',
+                'Some purple flowers',
+                'Egyptian art',
+                'A butterfly'];
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < arr.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', 'images/' + arr[i]);
+    newImage.setAttribute('alt', altArr[i]);
+    thumbBar.appendChild(newImage);
+    
+    // Add a click event listener to each <img> inside 
+    newImage.addEventListener('click', function(e) {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function(e) {
+    // Checks the current class name set on the <button> — you can again achieve this by using getAttribute().
+    if (btn.getAttribute('class') == 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken'
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
