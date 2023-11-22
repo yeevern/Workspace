a235e5b451d83a3f4f8e29a2dffe7a0fc997e2c1
yeevern <yeevernchang@gmail.com>
2023-11-22 16:35:28 +0800

    playing with some code

--- React/project/src/index.js
+++ React/project/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Gallery from './Gallery';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Gallery />
   </React.StrictMode>
 );
 
